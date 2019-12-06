7a3422db87323ace9886c5eb6a5a405e89e04880
bajrushivlora <bajrushivlora@gmail.com>
2019-12-06 02:02:45 -0500

    l

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+function randomNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 console.log("local storage demo file");
-
-
-
-
-//check if localstorage items exist 
 var checkForItems = function(){
 
   if (!storageAvailable('localStorage')){ return; }
-  
   if(localStorage.getItem('visibility')){
 		var visible = localStorage.getItem('visibility');
     console.log(visible);
@@ -21,13 +20,28 @@ var checkForItems = function(){
     document.querySelector(".plate_7").style.visibility = 'visible';
     document.querySelector(".plate_8").style.visibility = 'visible';
     }
+
+    if(localStorage.getItem('transform')){
+      var theScale = localStorage.getItem('transform');
+      console.log(theScale);
+
+      document.querySelector(".plate").style.transform = "scale(theScale)";
+      document.querySelector(".plate_2").style.transform = "scale(theScale)";
+      document.querySelector(".plate_3").style.transform = "scale(theScale)";
+      document.querySelector(".plate_4").style.transform = "scale(theScale)";
+      document.querySelector(".plate_5").style.transform = "scale(theScale)";
+      document.querySelector(".plate_6").style.transform = "scale(theScale)";
+      document.querySelector(".plate_7").style.transform = "scale(theScale)";
+      document.querySelector(".plate_8").style.transform = "scale(theScale)";
+}
 }
 
 
 // save the data to the website 
 var saveUserInfo = function(){
 
-	document.querySelector(".flexbox-buttons_spoon").addEventListener('click', displayFood);
+  // one
+	document.querySelector(".flexbox-buttons_start").addEventListener('click', displayFood);
     
     function displayFood() {
       var visible = 'visible';
@@ -42,10 +56,26 @@ var saveUserInfo = function(){
       document.querySelector(".plate_7").style.visibility = 'visible';
       document.querySelector(".plate_8").style.visibility = 'visible';
   }
+
+  //two 
+  document.querySelector(".flexbox-buttons_spoon").addEventListener('click', resizeFood);
+    
+  function resizeFood() {
+    var theScale = randomNumber(0, 1.8);
+    localStorage.setItem('transform', theScale);
+
+    document.querySelector(".plate").style.transform = "scale(theScale)";
+    document.querySelector(".plate_2").style.transform = "scale(theScale)";
+    document.querySelector(".plate_3").style.transform = "scale(theScale)";
+    document.querySelector(".plate_4").style.transform = "scale(theScale)";
+    document.querySelector(".plate_5").style.transform = "scale(theScale)";
+    document.querySelector(".plate_6").style.transform = "scale(theScale)";
+    document.querySelector(".plate_7").style.transform = "scale(theScale)";
+    document.querySelector(".plate_8").style.transform = "scale(theScale)";
+  }
 }
 
 
-//run functions when you refresh the page 
 window.addEventListener('DOMContentLoaded', function(){
 	checkForItems();
 	saveUserInfo(); 
